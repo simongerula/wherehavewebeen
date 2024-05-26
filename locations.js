064f98da917cd0dd04930999d834f95066598fbe
Simon Gerula <simongerula@Simons-MacBook-Air.local>
2024-05-26 16:33:52 +1200

    fix issue today date timezone to iso

--- locations.js
+++ locations.js
@@ -1,16 +1,13 @@
 const locations = [
     [-40.893762, 174.978244], 
-    [-40.785002, 175.151378], 
+    [-40.785002, 175.151378],
+    [-41.296739, 174.833011], 
 ];
 
 function getTodaysIndex() {
     const startDate = new Date("2024-05-25"); // Starting date
-    console.log(startDate)
     const today = new Date();
-    console.log(today)
     const diffTime = Math.abs(today - startDate);
-    console.log(diffTime)
     const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
-    console.log(diffDays)
     return diffDays;
 }
