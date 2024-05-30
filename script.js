d5d8aeb786506c4774ee08a8384eb2b7467ebdda
Simon Gerula <saimonsir@gmail.com>
2024-05-30 20:34:43 +1200

    word fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const correctWord = 'APPLE';
+    const correctWord = 'WHALE';
     const maxTries = 3;
     let currentAttempt = [];
     let attempts = JSON.parse(localStorage.getItem('attempts')) || [];
