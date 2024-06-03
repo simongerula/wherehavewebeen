635d440bf445b8622f35434dc475303f067e0729
Simon Gerula <saimonsir@gmail.com>
2024-06-03 22:21:57 +1200

    song and new worldle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const correctWord = 'BOMBS';
+    const correctWord = 'SHARE';
     const maxTries = 3;
     let currentAttempt = [];
     let attempts = JSON.parse(localStorage.getItem('attempts')) || [];
