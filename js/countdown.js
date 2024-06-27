9cd589d375ae175f49cada9b936e76e861e34c36
Simon Gerula <saimonsir@gmail.com>
2024-06-27 15:44:26 +1200

    Update holidays

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,16 @@
 function updateCountdown() {
-    const targetDate = new Date('June 28, 2024 00:00:00').getTime();
+    const targetDate = new Date('June 26, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const difference = targetDate - now;
+
+    if(difference <= 0){
+        clearInterval(intervalId);
+        document.getElementById('weDidIt').style.display = 'block'
+        document.getElementById('title').innerText = 'We did it love'
+        document.getElementById('countdown').style.display = 'none'
+        document.getElementById('theGif').style.display = 'none'
+
+    }
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -14,5 +23,5 @@ function updateCountdown() {
     document.getElementById('seconds').innerText = String(seconds).padStart(2, '0');
 }
 
-setInterval(updateCountdown, 1000);
+const intervalId = setInterval(updateCountdown, 1000);
 updateCountdown();
