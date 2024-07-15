1b0391e1fa9c042be0c6cbecaa2adccd1aacc226
Simon Gerula <saimonsir@gmail.com>
2024-07-15 22:17:00 +1200

    I love you endlessly

--- js/worldle.js
+++ js/worldle.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const correctWord = 'GRACE';
+    const correctWord = 'DAILY';
     const maxTries = 5;
     let currentAttempt = [];
     let attempts = JSON.parse(localStorage.getItem('attempts')) || [];
