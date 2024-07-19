e35074524fc779d41ff3996d7fb9cccab3e46caf
Simon Gerula <saimonsir@gmail.com>
2024-07-19 23:00:25 +1200

    update

--- js/worldle.js
+++ js/worldle.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const correctWord = 'MADRE';
+    const correctWord = 'SWIFT';
     const maxTries = 5;
     let currentAttempt = [];
     let attempts = JSON.parse(localStorage.getItem('attempts')) || [];
